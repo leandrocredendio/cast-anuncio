32476747ade24e08825dede2f73c536e442955e6
Leandro Rodrigues Credendio <leandro.credendio@castmeta.local>
2018-09-27 16:09:06 -0300

    modal visualizar

--- src/app/components/home/home-card-anuncio/home-card-anuncio.component.ts
+++ src/app/components/home/home-card-anuncio/home-card-anuncio.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Anuncio } from '../../../models/anuncio.model';
+import { VisualizarAnuncioComponent } from '../../../shared/visualizar-anuncio/visualizar-anuncio.component';
 
 @Component({
   selector: 'app-home-card-anuncio',
@@ -17,7 +18,9 @@ export class HomeCardAnuncioComponent implements OnInit {
   }
 
   public visualizarAnuncio(anuncio: Anuncio): void {
+    console.log("1 -> " + anuncio.id + " - " + this.anuncio.id);
     this.anuncio = anuncio;
+    console.log("2 -> " + anuncio.id + " - " + this.anuncio.id);
     this.exibeVisualizar = true;
   }
 
